fde31fd5e74aecb892ac63e101ed865be8d975b7
satya chaitanya <satyadev75@gmail.com>
2018-11-09 21:41:20 +0530

    added 1006

---
+++ 1006/DB/1_query.sql
@@ -0,0 +1,5 @@
+use classicmodels;
+select * from customers where customerNumber=496;
+select * from customers where customerNumber=500;
+commit;
+
---
+++ 1006/DB/3_query_prod.sql
@@ -0,0 +1,3 @@
+use classicmodels;
+select productCode from products where productLine="Motorcycles";
+select productLine from product where productName="1968 Ford Mustang";
